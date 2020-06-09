9fba4c621e4876e39709fdceac5c6ad9c776d5a7
hvirmani <harshit0030.cse19@chitkara.edu.in>
2020-06-09 23:04:22 +0530

    Final Commit

--- file.js
+++ file.js
@@ -17,11 +17,13 @@
             for (var j = 0; j < 4; j++) {
                 $(".ask").text("Question " + (i) + " of 5");
                 var y = j + 1;
-                $("." + y).text(arr[i - 1].answer[j]);
+
+                $("." + String.fromCharCode('a'.charCodeAt(0) + j)).text(arr[i - 1].answer[j]);
                 $("." + j).attr("id", "");
             }
             $("." + arr[0].correct).attr("id", "correct");
             $(".next").click(function () {
+                $(".active").css("background", "white");
                 if (i < arr.length) {
                     $(".question").text(arr[i].question);
                     ++i;
@@ -29,20 +31,24 @@
                     for (var j = 0; j < 4; j++) {
                         $(".ask").text("Question " + (i) + " of 5");
                         var y = j + 1;
-                        $("." + y).text(arr[i - 1].answer[j]);
+                        $("." + String.fromCharCode('a'.charCodeAt(0) + j)).text(arr[i - 1].answer[j]);
                         $("." + j).attr("id", "");
                     }
                     $("." + arr[i - 1].correct).attr("id", "correct");
+                    $(".result").text("");
                 } else {
-                    alert("Congrats you ended the quiz");
+                    alert(98);
                 }
             });
             $(".active").click(function () {
                 //alert($(this).attr("id"));
+                $(".active").css("background", "white");
                 if ($(this).attr("id") === "correct") {
-                    $(".result").text("Great You Gave The Right Answer");
+                    $(".result").text("Great you give the right answer");
+                    $(this).css("background", "#90ee90");
                 } else {
-                    $(".result").text("!Oops You Are Wrong!");
+                    $(".result").text("!Oops you give wrong answer!");
+                    $(this).css("background", "#ff5a57");
                 }
 
             });
